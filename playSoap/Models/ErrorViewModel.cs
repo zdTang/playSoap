4c6a84c649be076404f2067a10c581f2cb0edf66
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2020-05-03 10:51:10 -0400

    Add project files.

---
+++ playSoap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace playSoap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ playSoap/Models/PlayService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace playSoap.Models
+{
+    
+    public class PlayService
+    {
+        public string ServiceURL { get; set; }
+
+        PlayService(string serviceURL)
+        {
+            ServiceURL=serviceURL;
+        }
+
+
+      
+
+
+
+        
+    }
+
+
+}
